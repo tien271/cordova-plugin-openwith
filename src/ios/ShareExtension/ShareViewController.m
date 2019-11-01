2534e474408620b2420848d603ba9379cc8cd110
Etienne Lemay <etienne@heliom.ca>
2019-11-01 13:19:06 -0400

    Fix extension crash on iOS 13 As seen on https://github.com/j3k0/cordova-plugin-openwith/pull/69

--- src/ios/ShareExtension/ShareViewController.m
+++ src/ios/ShareExtension/ShareViewController.m
@@ -61,17 +61,30 @@
       NSMethodSignature *methodSignature = [responder methodSignatureForSelector:selector];
       NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
 
-      // Arguments
-      NSDictionary<NSString *, id> *options = [NSDictionary dictionary];
       void (^completion)(BOOL success) = ^void(BOOL success) {};
 
-      [invocation setTarget: responder];
-      [invocation setSelector: selector];
-      [invocation setArgument: &url atIndex: 2];
-      [invocation setArgument: &options atIndex:3];
-      [invocation setArgument: &completion atIndex: 4];
-      [invocation invoke];
-      break;
+      if (@available(iOS 13.0, *)) {
+        UISceneOpenExternalURLOptions * options = [[UISceneOpenExternalURLOptions alloc] init];
+        options.universalLinksOnly = false;
+
+        [invocation setTarget: responder];
+        [invocation setSelector: selector];
+        [invocation setArgument: &url atIndex: 2];
+        [invocation setArgument: &options atIndex:3];
+        [invocation setArgument: &completion atIndex: 4];
+        [invocation invoke];
+        break;
+      } else {
+        NSDictionary<NSString *, id> *options = [NSDictionary dictionary];
+
+        [invocation setTarget: responder];
+        [invocation setSelector: selector];
+        [invocation setArgument: &url atIndex: 2];
+        [invocation setArgument: &options atIndex:3];
+        [invocation setArgument: &completion atIndex: 4];
+        [invocation invoke];
+        break;
+      }
     }
   }
 }
