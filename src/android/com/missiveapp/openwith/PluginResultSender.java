39611c315502326d47ac179ff1ebd8d7d9c81f13
Tien Nguyen <>
2021-03-10 15:57:53 +0700

    fix share files on Android

--- src/android/com/missiveapp/openwith/PluginResultSender.java
+++ src/android/com/missiveapp/openwith/PluginResultSender.java
@@ -8,38 +8,33 @@ import org.apache.cordova.PluginResult;
  */
 @SuppressWarnings("PMD.ShortMethodName")
 class PluginResultSender {
-  /** Send an INVALID_ACTION error. */
-  public static boolean invalidAction(
-          final CallbackContext context)
-  {
-    final PluginResult result = new PluginResult(PluginResult.Status.INVALID_ACTION);
-    context.sendPluginResult(result);
 
-    return false;
-  }
+    /** Send an INVALID_ACTION error. */
+    public static boolean invalidAction(
+            final CallbackContext context) {
+        final PluginResult result = new PluginResult(PluginResult.Status.INVALID_ACTION);
+        context.sendPluginResult(result);
+        return false;
+    }
 
-  /** Send NO_RESULT.
-   * We generally keep the callback for a later call,so this is left as
-   * an option to this method. */
-  public static boolean noResult(
-          final CallbackContext context,
-          final boolean keepCallback)
-  {
-    final PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+    /** Send NO_RESULT.
+     * We generally keep the callback for a later call,so this is left as
+     * an option to this method. */
+    public static boolean noResult(
+            final CallbackContext context,
+            final boolean keepCallback) {
+        final PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        result.setKeepCallback(keepCallback);
+        context.sendPluginResult(result);
+        return true;
+    }
 
-    result.setKeepCallback(keepCallback);
-    context.sendPluginResult(result);
-
-    return true;
-  }
-
-  /** Send OK. */
-  public static boolean ok(
-          final CallbackContext context)
-  {
-    final PluginResult result = new PluginResult(PluginResult.Status.OK);
-    context.sendPluginResult(result);
-
-    return true;
-  }
+    /** Send OK. */
+    public static boolean ok(
+            final CallbackContext context) {
+        final PluginResult result = new PluginResult(PluginResult.Status.OK);
+        context.sendPluginResult(result);
+        return true;
+    }
 }
+// vim: ts=4:sw=4:et
